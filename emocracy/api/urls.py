7bf27c9dd79d8d30790ed0ebb396324f060d528a
Thijs <thijscoenen@computer-van-thijs-coenen.local>
2009-03-06 20:30:32 +0100

    Small bugfix.

--- emocracy/api/urls.py
+++ emocracy/api/urls.py
@@ -6,10 +6,10 @@ from views import UserResource, UserCollection
 
 
 urlpatterns = patterns('api.views',
-    url(r'^issue/$', IssueCollection(), name = 'api_issue'),
-    url(r'^issue/(?P<pk>\d+)/$', IssueResource(), name = 'api_issue_pk'),
-    url(r'^vote/$', VoteCollection(), name = 'api_vote'),
-    url(r'^vote/(?P<pk>\d+)/$', VoteResource(), name = 'api_vote_pk'),
-    url(r'^user/$', UserCollection(), name = 'api_user'),
-    url(r'^user/(?P<pk>\d+)/$', UserResource(), name = 'api_user_pk'),
+    url(r'^issues/$', IssueCollection(), name = 'api_issue'),
+    url(r'^issues/(?P<pk>\d+)/$', IssueResource(), name = 'api_issue_pk'),
+    url(r'^votes/$', VoteCollection(), name = 'api_vote'),
+    url(r'^votes/(?P<pk>\d+)/$', VoteResource(), name = 'api_vote_pk'),
+    url(r'^users/$', UserCollection(), name = 'api_user'),
+    url(r'^users/(?P<pk>\d+)/$', UserResource(), name = 'api_user_pk'),
 )
