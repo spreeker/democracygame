5789947e214db5a4a72dc9b30d9ec2f6651a6a19
Thijs <thijscoenen@30-135.surfsnel.dsl.internl.net>
2009-03-10 17:28:05 +0100

    Added the handling of private votes to the no javascript fallback of the voting system. Also added some comments.

--- emocracy/web/context_processors.py
+++ emocracy/web/context_processors.py
@@ -1,7 +1,10 @@
 # by Thijs Coenen for the Emocracy project october 2008
-#from emocracy.accounts.views import create_userprofile
 
 def profile(request):
+    """This context processor adds a user's userprofile to the context if a user
+    is logged in."""
+    # This context processor is used by the RequestContext, see the settings.py
+    # TEMPLATE_CONTEXT_PROCESSORS variable for the other context processors.
     if request.user.is_authenticated():
         return {'userprofile' : request.user.get_profile()}
     else:
