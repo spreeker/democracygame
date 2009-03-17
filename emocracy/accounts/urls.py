e0b933e0fd3c176a6322c8770eeae7fc8f48613e
stephan <stephan@subuntu.(none)>
2009-03-17 16:48:11 +0100

    work in progress on new accounts registering using mail

--- emocracy/accounts/urls.py
+++ emocracy/accounts/urls.py
@@ -2,14 +2,59 @@ import settings
 from django.conf.urls.defaults import *
 from django.views.generic.simple import redirect_to
 
+from django.views.generic.simple import direct_to_template
+from django.contrib.auth import views as auth_views
+
+from registration.views import activate
+from registration.views import register
+
+#urlpatterns = patterns('',
+#    url(r'^register/$', 'emocracy.accounts.views.register_user', name = 'register'),
+#)
+
 
 urlpatterns = patterns('',
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name' : 'accounts/login.html' }, name = 'login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name' : 'accounts/logged_out.html'}, name = 'logout'),
-    url(r'^register/$', 'emocracy.accounts.views.register_user', name = 'register'),
-    url(r'^password_reset/$', 'emocracy.accounts.views.email_new_password', name = 'email_new_password'),
-    url(r'^profile/(?P<username>\w+)/$', 'emocracy.accounts.views.userprofile_show', name = 'userprofile'),
-    url(r'^change_description/$', 'emocracy.accounts.views.change_description', name = 'change_description'),
-    url(r'^search_user/$', 'emocracy.accounts.views.search_user', name = 'search_user'),
-#    (r'$', redirect_to, {'url' : '/web/issues/'}),
-)
+       # Activation keys get matched by \w+ instead of the more specific
+       # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+       # that way it can return a sensible "invalid key" message instead of a
+       # confusing 404.
+	   #url(r'^register/$', 'emocracy.accounts.views.register_user', 
+       #	   name = 'register'),
+       url(r'^activate/(?P<activation_key>\w+)/$',
+           activate,
+           name='registration_activate'),
+       url(r'^login/$',
+           auth_views.login,
+           {'template_name': 'accounts/login.html'},
+           name='auth_login'),
+       url(r'^logout/$',
+           auth_views.logout,
+           {'template_name': 'accounts/logout.html'},
+           name='auth_logout'),
+       url(r'^password/change/$',
+           auth_views.password_change,
+           name='auth_password_change'),
+       url(r'^password/change/done/$',
+           auth_views.password_change_done,
+           name='auth_password_change_done'),
+       url(r'^password/reset/$',
+           auth_views.password_reset,
+           name='auth_password_reset'),
+       url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+           auth_views.password_reset_confirm,
+           name='auth_password_reset_confirm'),
+       url(r'^password/reset/complete/$',
+           auth_views.password_reset_complete,
+           name='auth_password_reset_complete'),
+       url(r'^password/reset/done/$',
+           auth_views.password_reset_done,
+           name='auth_password_reset_done'),
+       url(r'^register/$',
+           register,
+           {'template_name' : 'registration/registration_form.html'},
+           name='registration_register'),
+       url(r'^register/complete/$',
+           direct_to_template,
+           {'template': 'registration/registration_complete.html'},
+           name='registration_complete'),
+       )
