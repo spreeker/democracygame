ec43149a6235879aac877abdec765f371478e47f
stephan <stephan@smtp.online.nl>
2009-04-20 10:24:06 +0200

    moved server settings to settings file

--- thirdparty/settings.py
+++ thirdparty/settings.py
@@ -23,6 +23,8 @@ DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
+EMOCRACY_API_SERVER = "http://emo.preeker.net:80/api/"
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
