ab0ab5f7de1e689e55bac1644e445c3be34f190d
Conrado <conrado@cobweb.buhrer.net>
2009-05-05 19:41:52 +0200

    forgot these

---
+++ thirdparty/oauth_consumer/utils.py
@@ -0,0 +1,24 @@
+import oauth
+from django.conf import settings
+
+import easyoauth
+import httplib
+
+
+SERVER = getattr(settings, 'OAUTH_SERVER', 'emo.buhrer.net')
+PORT = getattr(settings, 'OAUTH_PORT', 80)
+
+SIGNATURE_METHOD = oauth.OAuthSignatureMethod_PLAINTEXT()
+
+OAUTH_URLS = easyoauth.OAuthURLS()
+OAUTH_URLS.request_token_url = '/oauth/request_token/'
+OAUTH_URLS.access_token_url = '/oauth/access_token/'
+OAUTH_URLS.authorization_url = 'http://emo.buhrer.net/oauth/authorize/'
+
+CALLBACK_URL = 'http://emo.buhrer.net:8000/oauth/callback/'
+RESOURCE_URL = '/api/issues/'
+
+CONSUMER_KEY = getattr(settings, 'CONSUMER_KEY', 'thirdparty')
+CONSUMER_SECRET = getattr(settings, 'CONSUMER_SECRET', 'thirdparty')
+
+
---
+++ thirdparty/profiles/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique = True)
+    signature = models.TextField(blank = True)
+    url = models.URLField(verify_exists = False, blank = True)
+    id_is_verified = models.BooleanField(default = False)
+    timezone = models.CharField(max_length = 100, default="Europe/London")
+    access_token = models.CharField(max_length=400, blank=True)
+    
