2e4c61be71dacc985bc05bfa26a64fe3fe40916b
Conrado Buhrer <conrado@cobweb.buhrer.net>
2009-05-07 18:33:44 +0200

    no changes

--- thirdparty/oauth_consumer/utils.py
+++ thirdparty/oauth_consumer/utils.py
@@ -5,7 +5,7 @@ import easyoauth
 import httplib
 
 
-SERVER = getattr(settings, 'OAUTH_SERVER', 'emo.buhrer.net')
+SERVER = getattr(settings, 'OAUTH_SERVER', 'emo.preeker.net')
 PORT = getattr(settings, 'OAUTH_PORT', 80)
 
 SIGNATURE_METHOD = oauth.OAuthSignatureMethod_PLAINTEXT()
@@ -13,9 +13,9 @@ SIGNATURE_METHOD = oauth.OAuthSignatureMethod_PLAINTEXT()
 OAUTH_URLS = easyoauth.OAuthURLS()
 OAUTH_URLS.request_token_url = '/oauth/request_token/'
 OAUTH_URLS.access_token_url = '/oauth/access_token/'
-OAUTH_URLS.authorization_url = 'http://emo.buhrer.net/oauth/authorize/'
+OAUTH_URLS.authorization_url = 'http://emo.preeker.net/oauth/authorize/'
 
-CALLBACK_URL = 'http://emo.buhrer.net:8000/oauth/callback/'
+CALLBACK_URL = 'http://emo.buhrer.net/oauth/callback/'
 RESOURCE_URL = '/api/issues/'
 
 CONSUMER_KEY = getattr(settings, 'CONSUMER_KEY', 'thirdparty')
