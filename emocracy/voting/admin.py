4c64f633dd96f98fb0eba0e68c54d6c8abdcd628
stephan <stephan@subuntu.(none)>
2009-05-15 16:50:16 +0200

    modyfied look and feel of admin and updated source code

--- emocracy/voting/admin.py
+++ emocracy/voting/admin.py
@@ -1,9 +1,17 @@
 # by Thijs Coenen  for the Emocracy project
 from django.contrib import admin
 
-from models import Tag, TaggedIssue, Issue, IssueSet
+from models import Tag
+from models import TaggedIssue
+from models import Issue
+from models import IssueSet
+
+class IssueAdmin(admin.ModelAdmin):
+    date_hierarchy = "time_stamp"
+    list_display = ('title', 'time_stamp', 'owner' , 'votes') 
+    list_filter = ( 'title' , 'time_stamp', 'owner' , 'votes')
+
 admin.site.register(Tag)
 admin.site.register(TaggedIssue)
-
 admin.site.register(IssueSet)
-admin.site.register(Issue)
+admin.site.register(Issue , IssueAdmin)
