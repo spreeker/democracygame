1cf786604583c9b1fc6a5e19519427760cd9196e
Conrado <conrado@cobweb.buhrer.net>
2009-05-16 14:01:40 +0200

    new api using django piston

--- emocracy/api/urls.py
+++ emocracy/api/urls.py
@@ -1,26 +1,29 @@
-import settings
 from django.conf.urls.defaults import *
-from views import IssueResource, IssueCollection 
-from views import IssueVoteCollection
-from views import IssueTagCollection
-from views import IssueVoteUserResource
-from views import VoteResource 
-from views import VoteCollection
-from views import UserResource
-from views import UserCollection
-from views import TagResource
+from piston.resource import Resource
+from piston.authentication import OAuthAuthentication
+from piston.emitters import Emitter
+from piston.emitters import JSONEmitter
 
+from emocracy.api.handlers import IssueHandler
+from emocracy.api.handlers import IssueListHandler
+from emocracy.api.handlers import UserListHandler
+from emocracy.api.handlers import UserHandler
 
-urlpatterns = patterns('api.views',
-    url(r'^issues/$', IssueCollection(), name = 'api_issue'),
-    url(r'^issues/(?P<pk>\d+)/$', IssueResource(), name = 'api_issue_pk'),
-    url(r'^issues/(?P<pk>\d+)/votes/$', IssueVoteCollection(), name = 'api_issue_pk_vote'),  
-    url(r'^issues/(?P<pk>\d+)/tags/$', IssueTagCollection(), name = 'api_issue_pk_tag'),  
-    url(r'^issues/(?P<issue_pk>\d+)/for_user/(?P<user_pk>\d+)/$', IssueVoteUserResource(), name = 'api_issue_pk_vote_user'),
-    url(r'^votes/$', VoteCollection(), name = 'api_vote'),
-    url(r'^votes/(?P<pk>\d+)/$', VoteResource(), name = 'api_vote_pk'),
-    url(r'^users/$', UserCollection(), name = 'api_user'),
-    url(r'^users/(?P<pk>\d+)/$', UserResource(), name = 'api_user_pk'),
-    url(r'^tags/(?P<pk>\d+)/$', TagResource(), name = 'api_tag_pk'),
-    url(r'^oauth_test/$', 'oauth_test', name = 'oauth_test'),
+# Emitter registrered to set wrong mime-type for visibility purposes
+# just comment these two lines out for production
+Emitter.unregister(JSONEmitter)
+Emitter.register('json', JSONEmitter, 'text/html; charset=utf-8')
+
+auth = OAuthAuthentication(realm='emo.buhrer.net')
+
+issues = Resource(handler=IssueListHandler, authentication=auth)
+issue = Resource(handler=IssueHandler, authentication=auth)
+users = Resource(handler=UserListHandler, authentication=auth)
+user = Resource(handler=UserHandler, authentication=auth)
+
+urlpatterns = patterns('',
+    url(r'^issues/$', issues),
+    url(r'^issues/(?P<id>\d+)/$', issue),
+    url(r'^users/$', users),
+    url(r'^users/(?P<id>\d+)/$', user),
 )
