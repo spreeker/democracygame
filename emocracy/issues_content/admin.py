84dd9a9dbcf2aa5c0fd1bcc808c353174832e0a4
spreeker <stephan@preeker.net>
2009-05-21 15:55:54 +0200

    fixed admin bugs

--- emocracy/issues_content/admin.py
+++ emocracy/issues_content/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 from models import IssueBody 
 
 
-class IssueBodyAdmin(admin.ModelAdmin)
+class IssueBodyAdmin(admin.ModelAdmin):
     fields = ( 'body' , 'url' , 'source_type' )
-    search_fields = ( 'owner' , 'title' )
-    
-admin.site.register(IssueBodyi, IssueBodyAdmin)
+ 
+admin.site.register(IssueBody, IssueBodyAdmin)
