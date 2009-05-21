9ea97f2ec4a6a9323fbce956085a80d4de57ecbe
Thijs <thijscoenen@computer-van-thijs-coenen.local>
2009-05-21 18:40:21 +0200

    Preliminary work on game rule refactor.

---
+++ emocracy/gamelogic/upgrade.py
@@ -0,0 +1,35 @@
+NUMBER_OF_OPINION_LEADERS = 10
+MIN_SCORE_ACTIVE_CITIZENS = 50
+
+def anonymous_citizen(user):
+    return 'anonymous citizen'
+
+def citizen(userprofile):
+    if user.userprofile.score > MIN_SCORE_ACTIVE_CITIZENS:
+        userprofile.role = 'active citizen'
+        userprofile.save()
+        return 'active citizen'
+    else:
+        return 'citizen'
+        
+def active_citizen(userprofile):
+    opinion_leaders = UserProfile.objects.filter(role = 'opinion leader').sort_by(-score)[:NUMBER_OF_OPINION_LEADERS]
+    if userprofile.score > opinion_leaders[-1].score:
+        userprofile.role = 'opinion leader'
+        userprofile.save()
+        to_be_downgraded = opinion_leaders[-1]
+        to_be_downgraded.role = 'active citizen'
+        to_be_downgraded.save()
+        return 'opinion leader'
+    else
+        return 'active citizen'
+
+def opinion_leader(user):
+    pass
+
+upgrade = {
+    'anonymous citizen' : {anonymous_citizen},
+    'citizen' : {citizen},
+    'active citizen' : {active_citizen},
+    'opinion leader' : {opinion_leader}
+}
