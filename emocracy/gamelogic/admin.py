0e17d56a67f48579d27a3a6b37b5fc8dc6908bd6
stephan <stephan@subuntu.(none)>
2009-05-21 15:55:54 +0200

    fixed admin bugs

--- emocracy/gamelogic/admin.py
+++ emocracy/gamelogic/admin.py
@@ -1,10 +1,11 @@
 # by Thijs Coenen  for the Emocracy project
 from django.contrib import admin
 
-from models import Mandate, LawProposal, Motion, IssueBody
+from models import Mandate
+from models import LawProposal
+from models import Motion
 
 
 admin.site.register(Mandate)
 admin.site.register(LawProposal)
 admin.site.register(Motion)
-admin.site.register(IssueBody)
