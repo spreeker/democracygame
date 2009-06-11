83d4b35afc0f46f0af3cfaba901ee21dd7b6bec8
Conrado <conrado@cobweb.buhrer.net>
2009-06-11 15:17:44 +0200

    mostly cosmetics

--- thirdparty/web/views.py
+++ thirdparty/web/views.py
@@ -11,10 +11,13 @@ from django.core import serializers
 from django.utils import simplejson as json
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
-
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 
 from oauth_consumer import EmoOAuthConsumerApp
+from web.forms import IssueFormNew
 
 emo = EmoOAuthConsumerApp()
 
@@ -32,12 +35,10 @@ class HttpResponseCreated(HttpResponse):
 
 
 def issues_list_hottest(request):
-    response = emo.get_issue_list(request)
-    data = response.read()
-    print data
-    extra_context = json.loads( data )
+    issue_list = emo.get_issue_list(request)
+    print issue_list
     fetch = []
-    for resource in extra_context:
+    for resource in issue_list:
         issueid = resource['issue_uri'].split('/')
         issueid = issueid[-2:]
         issueid = issueid[0]
@@ -74,73 +75,7 @@ def issues_list_hottest(request):
             RequestContext(request, context))
 
 def issues_list_popular(request):
-    #request.user.is_authenticated()
-    try:
-        page = int(request.GET.get('page', '1'))
-    except ValueError:
-        page = 1
-    req = urllib2.Request(EMOCRACY_API_SERVER+"issues/?page=%s" %page)
-    try:
-        response = urllib2.urlopen(req)
-    except HTTPError, e:
-        if __debug__:
-            print 'The server couldn\'t fulfill the request.'
-            print 'Error code: ', e.code
-    except URLError, e:
-        if __debug__:
-            print 'We failed to reach the server.'
-            print 'Reason: ', e.reason
-        return url_error_view(request, e)
-    else:
-        pass
-    data = response.read()
-    #print data
-    extra_context = json.loads( data )
-    #print extra_context
-    #if extra_context.has_key('next'):
-    #    print 'next: ' + extra_context['next']
-    #    next = extra_context['next']
-    #    next = next.split('/')[-1:]
-    #    extra_context['next'] = next[0]
-    #else:
-    #    extra_context['next'] = ''
-    #if extra_context.has_key('previous'):
-    #    print 'previous: ' + extra_context['previous']
-    #    previous = extra_context['previous']
-    #    previous = previous.split('/')[-1:]
-    #    extra_context['previous'] = previous[0]
-    #else:
-    #    extra_context['previous'] = ''
-    #print extra_context
-    fetch = []
-    for resource in extra_context:
-        issueid = resource['issue_uri'].split('/')
-        issueid = issueid[-2:]
-        issueid = issueid[0]
-        resource_data = {}
-        resource_data['id'] = issueid
-        resource_datetime = datetime.datetime.strptime(resource['time_stamp'],"%Y-%m-%d %H:%M:%S")
-        now = datetime.datetime.now()
-        dt = now - resource_datetime
-        print resource
-        resource_data['title'] = resource['title']
-        resource_data['body'] = resource['body']
-        resource_data['votes_for'] = resource['votes_for']
-        resource_data['votes_abstain'] = resource['votes_abstain']
-        resource_data['votes_against'] = resource['votes_against']
-        if not dt.days:
-            resource_data['age'] = 'Today'
-        else:
-            resource_data['age'] = '%s days ago' % dt.days
-        resource_data['user'] = resource['owner']['username']
-        userid = resource['owner']['user_uri'].split('/')
-        userid = userid[-2:]
-        resource_data['userid'] = userid[0]
-        fetch.append(resource_data)
-    context = {}
-    context['issues'] = fetch
-    return render_to_response('issue_list.html', 
-            RequestContext(request, context))
+    return issues_list_hottest(request)
 
 def issues_issue_detail(request, pk):
     req = urllib2.Request(settings.EMOCRACY_API_SERVER+"issues/%s/" % pk)
@@ -157,7 +92,77 @@ def issues_issue_detail(request, pk):
             RequestContext(request, extra_context))
 
 def issues_list_newest(request):
-    return issues_list_popular(request)
+    return issues_list_hottest(request)
+
+
+@login_required
+def issues_add_issue(request, issue_no=None):
+    """
+    This view handles proposing new issues.
+    """
+    if issue_no is not None: # view was called to edit an issue
+        issue = emo.get_issue(request, issue_no)
+        print issue
+        #issue = get_object_or_404(Issue, pk = issue_no)
+        try:
+            # TODO
+            # If users were to be allowed to change their opinion on their own
+            # issue the following line needs to use a filter() instead of get().
+            v = Vote.objects.get(issue = issue, owner = request.user)
+            owners_vote_int = v.vote_int
+        except:
+            owners_vote_int = 1
+        if issue.owner != request.user: # check that user is owner
+            # TODO add is_draft check
+            # TODO add more appropriate Http Response code if user tries to
+            # change an issue that is not theirs.
+            raise Http404
+        # TODO check type of payload in some future where there might be more
+        # than just IssueBody objects linked to Issue objects.
+        issue_body = issue.payload
+        # note the following ties into the IssueFormNew definition in web/forms.py
+        data = {
+            'title' : issue.payload.title,
+            'body' : issue.payload.body,
+            'url' : issue.payload.url,
+            'source_type' : issue.payload.source_type,
+            'owners_vote' : owners_vote_int,
+            'is_draft': issue.is_draft
+        }
+
+    if request.method == "POST":
+        form = IssueFormNew(request.POST)
+        if form.is_valid():
+            status = emo.post_issue(
+                request,
+                {
+                    'title' : form.cleaned_data[u'title'],
+                    'body' : form.cleaned_data[u'body'],
+                    'owners_vote' : form.cleaned_data[u'owners_vote'],
+                    'url' : form.cleaned_data[u'url'],
+                    'source_type' : form.cleaned_data[u'source_type'],
+                    'is_draft' : form.cleaned_data[u'is_draft'],
+                    'issue_no' : issue_no,
+                }
+            )
+
+            #return HttpResponseRedirect(reverse('web_issue_detail', args = [new_issue.pk]))
+            if status == 200:
+                return HttpResponseRedirect(reverse('top_level_menu'))
+            else:
+                return HttpResponseRedirect(reverse('issues_submit_error'))
+    else:
+        if issue_no == None:
+            form = IssueFormNew()
+        else:
+            form = IssueFormNew(data)
+
+    context = RequestContext(request, {"form" : form})
+    return render_to_response("new_issue.html", context)
+ 
+def issues_submit_error(request):
+    context = RequestContext(request, {})
+    return render_to_response("issues_submit_error.html", context)
 
 def user_logout(request):
     return render_to_response('login.html')
