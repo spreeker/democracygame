779da159f98b9e95900b0a05faba3bce9e537c7e
stephan <stephan@quad.(none)>
2009-06-29 12:36:25 +0200

    refactoring api , not done yet

--- emocracy/api/urls.py
+++ emocracy/api/urls.py
@@ -8,11 +8,10 @@ from piston.emitters import Emitter
 from piston.emitters import JSONEmitter
 
 from emocracy.api.handlers import IssueHandler
-from emocracy.api.handlers import IssueListHandler
-from emocracy.api.handlers import UserListHandler
 from emocracy.api.handlers import UserHandler
 from emocracy.api.handlers import VoteHandler
-from emocracy.api.handlers import VoteListHandler
+
+from emocracy.api.handlers import IssueVotesHandler
 
 auth = OAuthAuthentication(realm='emo.buhrer.net')
 
@@ -26,18 +25,23 @@ if settings.DEBUG:
     Emitter.register('json', JSONEmitter, 'text/html; charset=utf-8')
     auth = HttpBasicAuthentication(realm='emo.buhrer.net')
 
-issues = Resource(handler=IssueListHandler, authentication=auth)
-issue = Resource(handler=IssueHandler, authentication=auth)
-users = Resource(handler=UserListHandler, authentication=auth)
-user = Resource(handler=UserHandler, authentication=auth)
-votes = Resource(handler=VoteListHandler, authentication=auth)
-vote = Resource(handler=VoteHandler, authentication=auth)
+issue = Resource( handler=IssueHandler, authentication=auth )
+user = Resource( handler=UserHandler, authentication=auth )
+vote = Resource( handler=VoteHandler, authentication=auth )
+
+issue_votes = Recource( handler=IssueVotesHandler)
 
 urlpatterns = patterns('',
-    url(r'^issues/$', issues , name="api_issues" ),
-    url(r'^issues/(?P<id>\d+)/$', issue ,  name="api_issue"),
-    url(r'^users/$', users , name="api_users" ),
-    url(r'^users/(?P<id>\d+)/$', user , name="api_user" ),
-    url(r'^votes/$', votes, name="api_votes" ),
-    url(r'^votes/(?P<id>\d+)/$', vote , name="api_vote" ),
+    # returns paginated issues public
+    url(r'^issue/$', issue , name="api_issues" ),
+    # returns specific issue public
+    url(r'^issue/(?P<id>\d+)/$', issue ,  name="api_issue"),
+    # return votes on specific issue public
+    url(r'^issue/(?P<id>\d+)/votes$', vote , name="api_issue_votes" ),
+    # return users not public
+    url(r'^user/$', user , name="api_users" ),
+    # return specific user 
+    url(r'^user/(?P<id>\d+)/$', user , name="api_user" ),
+
+    url(r'^vote/$', votes , name="api_votes" ),
 )
