2c7b4ccfb6caa3f72a0380a73de76f785086dd5f
stephan preeker <stephan@quad.(none)>
2009-07-06 21:03:04 +0200

    ALL 95 TEST ARE OK , trashed the web interface

---
+++ emocracy/api/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from voting.models import normal_votes
+from voting.models import Issue
+from issues_content.models import source_types
+
+class IssueForm(forms.Form):
+    title = forms.CharField(max_length = 100)
+    body = forms.CharField(
+        widget = forms.Textarea(),
+        max_length = 2000,
+    )
+    url = forms.URLField()
+    source_type = forms.CharField( widget = forms.ChoiceField( choices = source_types ))
+    owners_vote = forms.ChoiceField( choices = normal_votes)
+
+    is_draft = forms.BooleanField(initial = True, required = False)
+
+
+class VoteForm(forms.Form):
+    vote = forms.ChoiceField (choices = normal_votes) 
+    keep_private = forms.BooleanField(initial = False , required = False )
+    issue = forms.ModelChoiceField ( queryset = Issue.objects.all() )
