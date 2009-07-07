1c67d0cca00a882fce357b710995491e3f8456a3
stephan <stephan@quad.(none)>
2009-07-07 17:09:44 +0200

    renamed emocracy to edemocracy

---
+++ edemocracy/gamelogic/models.py
@@ -0,0 +1,47 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+from edemocracy.voting.models import Issue
+
+tag_count_threshold = 1
+MAX_MULTIPLIERS = 4
+
+roles = ["anonymous citizen", 
+        "citizen", 
+        "active citizen", 
+        "opinion leader", 
+        "candidate", 
+        "parliament member", 
+        "minister", 
+        'prime minister']
+
+class MultiplyIssue(models.Model):
+    user = models.ForeignKey(User )
+    time_stamp = models.DateTimeField(auto_now_add = True)
+    issue = models.ForeignKey( Issue )
+    downgrade = models.BooleanField( default = False )
+    multiply_value = models.IntegerField( blank=True , null=True )
+
+    def __unicode__(self):
+        return _(u'%(user)s  multiplies for %(issue)s' % {
+            'user' : self.user.username,
+            'issue' : self.issue.title, 
+            })
+
+    def save(self, force_insert=False, force_update=False):
+        """ Multiply an issue
+            only if your limit is not reached.
+
+            Maybe this can be extended for users at different levels to have different multiplies
+        """
+        msg = "Failed to add Multiply"
+        count_m = MultiplyIssue.objects.filter( user = self.user ).count()
+        if count_m < MAX_MULTIPLIERS:
+            if not self.issue.owner == self.user:
+                super(MultiplyIssue , self).save(force_insert, force_update)
+                msg = "succes fully added multiply" 
+
+        self.user.message_set.create(message=msg)
+
+
---
+++ edemocracy/web/forms.py
@@ -0,0 +1,148 @@
+from edemocracy.voting.models import blank_votes
+from edemocracy.voting.models import Tag
+from edemocracy.voting.models import normal_votes
+from edemocracy.issues_content.models import source_types
+
+from django import forms
+from django.forms import ModelForm
+from django.utils.translation import ugettext as _
+
+possible_motivations = [x[0] for x in blank_votes]
+
+class AuthorizeRequestTokenForm(forms.Form):
+    authorize_access = forms.IntegerField(
+        initial = 1,
+        widget = forms.HiddenInput(attrs = {'id' : 'authorize_access'})
+        )
+
+class HiddenOkForm(forms.Form):
+    ok = forms.IntegerField(initial = 1, widget = forms.HiddenInput())
+
+class IssueFormNew(forms.Form):
+    title = forms.CharField(max_length = 100)
+    body = forms.CharField(
+        widget = forms.Textarea(),
+        max_length = 2000,
+    )
+    url = forms.URLField()
+    source_type = forms.CharField(
+        widget = forms.Select(choices = source_types)
+    )
+    owners_vote = forms.IntegerField(widget = forms.Select(
+        choices = normal_votes,
+    ))
+    is_draft = forms.BooleanField(initial = True, required = False)
+
+class TagForm(forms.Form):
+    tags = forms.RegexField(
+            regex = "^[a-z]+$" ,
+            max_length = 50 ,
+            error_messages = { 'invalid' : "please enter a single lowercase tag word" },
+    )
+    issue_no = forms.IntegerField(widget = forms.HiddenInput())
+
+def tag_selection_helper():
+    qs = Tag.objects.get_popular(10)
+    l = [(u'', u'--------')]
+    l.extend([(unicode(x), unicode(x)) for x in qs])
+    return l
+
+class TagForm2(forms.Form):
+    """Form used by the Javascript less version of the tagging system."""
+    
+    tags = forms.CharField(max_length = 50, required = False)
+    def __init__(self, *args, **kwargs):
+        """Initializes form instances, is needed to be able to set the popular
+        tags field dynamically (on a per instance basis)."""
+        super(TagForm2, self).__init__(*args, **kwargs)
+        self.fields['popular_tags'] = forms.ChoiceField(choices = tag_selection_helper(), required=False)
+    
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        if not cleaned_data['tags'] and not cleaned_data['popular_tags']:
+            raise forms.ValidationError(u'No tag selected or given.')            
+        else:
+            return cleaned_data
+
+class TagSearchForm(forms.Form):
+    search_string = forms.CharField(max_length = 50, initial = _(u'Tag'))
+
+class NormalVoteForm(forms.Form):
+    vote = forms.IntegerField(widget = forms.Select(
+        choices = [(u"-1", _(u"Against")), (u"0", _(u"Blank...")), (u"1", _(u"For"))]
+    ))
+    keep_private = forms.BooleanField(required = False, initial = False)
+
+class BlankVoteForm(forms.Form):
+    motivation = forms.IntegerField(widget = forms.Select(
+        choices = blank_votes
+    ))
+    keep_private = forms.BooleanField(required = False, initial = False)
+
+class CastVoteFormFull(forms.Form):
+    """
+    Full vote form. (Allows both motivated blank votes or unmotivated 'for' or 
+    'against' votes.)
+    """
+    vote = forms.IntegerField(widget = forms.Select(
+        choices = [(u"-1", _(u"Against")), (u"0", _(u"Blank...")), (u"1", _(u"For"))]
+    ))
+    motivation = forms.IntegerField(widget = forms.Select(
+        choices = blank_votes
+    ))
+    issue_no = forms.IntegerField(widget = forms.HiddenInput())
+    keep_private = forms.BooleanField(required = False)
+    # second_step is used for non AJAX submissions (to show or not show certain
+    # fields)
+    second_step = forms.BooleanField(required = False, initial = False, widget = forms.HiddenInput())
+    
+    def clean(self):
+        """
+        Protect against garbage form data. This function checks that: 
+        -blank votes are motivated
+        -the votes are either for, against or blank
+        This function also sets the motivation of a 'for' or 'against' vote
+        to an empty string and defaults the keep_private variable to False if
+        it is not available.
+        """
+                
+        cleaned_data = self.cleaned_data
+                
+        try:
+            vote_int = cleaned_data['vote']
+        except KeyError:
+            #print 'No vote'
+            raise forms.ValidationError(u'No vote')
+
+        if not vote_int in [-1, 0, 1]:
+            #print u'Invalid vote'
+            raise forms.ValidationError(u'Invalid vote')
+        
+        try:
+            issue_no = cleaned_data['issue_no']
+        except KeyError:
+            #print 'No issue'
+            raise forms.ValidationError(u'No issue')
+        
+        try:
+            motivation = cleaned_data['motivation']
+        except KeyError:
+            motivation = None
+
+        if vote_int == 0:
+            if motivation not in possible_motivations:
+                #print 'Invalid motivation'
+                raise forms.ValidationError(u'Invalid motivation')
+        else: 
+            # We already know vote_int to be in [-1, 0, 1] so here we have 
+            # vote_int in [-1, 1] -> obliterate the motivation.
+            # TODO check wether this is a redundant check.
+            cleaned_data[u'motivation'] = 0
+        
+        try:
+            keep_private = cleaned_data[u'keep_private']
+        except KeyError:
+            #print "keep_private reset"
+            cleaned_data[u'keep_private'] = False
+
+        return cleaned_data
