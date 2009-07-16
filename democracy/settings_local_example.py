fcbe1d6cd635c6def2fbd433a70a069422912c90
spreeker <stephan@preeker.net>
2009-07-16 22:19:31 +0200

    new default db settings

--- democracy/settings_local_example.py
+++ democracy/settings_local_example.py
@@ -6,11 +6,11 @@ SECRET_KEY = 'SERVER SPECIFIC DO NOT SHARE!'
 
 
 DATABASE_ENGINE = 'postgresql_psycopg2' 
-DATABASE_NAME = 'emo_develop'
+DATABASE_NAME = 'd_develop'
 
-DATABASE_USER = 'emo'             
-DATABASE_PASSWORD = 'abctreehouse'
+DATABASE_USER = 'demo'             
+DATABASE_PASSWORD = 'demo'
 DATABASE_HOST = 'preeker.net'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
-TEST_DATABASE_NAME = 'emo_test'
+TEST_DATABASE_NAME = 'demo_test'
