d8bf215fe077cb2d102d8756a1cd177473195c84
Stephan Preeker <stephan@preeker.net>
2009-07-17 18:16:44 +0200

    piston update

---
+++ external_apps/django-piston/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
+    
+import os
+
+setup(
+    name = "django-piston",
+    version = "0.2.2",
+    url = 'http://bitbucket.org/jespern/django-piston/wiki/Home',
+	download_url = 'http://bitbucket.org/jespern/django-piston/downloads/',
+    license = 'BSD',
+    description = "Piston is a Django mini-framework creating APIs.",
+    author = 'Jesper Noehr',
+    author_email = 'jesper@noehr.org',
+    packages = find_packages(),
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
