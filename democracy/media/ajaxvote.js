e5c1ffa10473c08dfe431b0f8baf558273252943
Stephan <stephan@preeker.net>
2009-07-21 14:29:06 +0200

    gave the dashboard interface a workable look

--- democracy/media/ajaxvote.js
+++ democracy/media/ajaxvote.js
@@ -1,15 +1,7 @@
 $(document).ready(function(){
 
-    $(".votelink").click(function(event){
-        // Clear all voteforms (they might have been added through javscript):
-        var clicked_votelink = $(this);
-        $(".voteform_target").html("");
+    $(".voteform").click(function(event){
 
-        // User requested a voteform. Hide the clicked votelink, unhide the
-        // others if present:
-        $(".votelink").css("visibility", "visible");
-        $(".taglink").css("visibility", "visible");
-        clicked_votelink.css("visibility", "hidden");
         // Find the number of the issue under consideration:
         var id_string = $(this).attr("id");
         var match = id_string.match(/^votelink_(\S*)$/);
@@ -19,16 +11,7 @@ $(document).ready(function(){
         // jQuery load function:
         var target_div = $("#voteform_" + issue_no);
         target_div.load("/web/ajax/voteform/" + issue_no + "/", function(){
-        
-            // Deal with normal votes and blank votes. This assumes the HTML 
-            // form that was received defaults to a blank vote. Only show the
-            // motivation dropdown for blank votes.
-            $("#id_vote").change(function(){
-                if ($(this).val() != 0){
-                    $("#id_motivation").fadeOut();
-                } else {
-                    $("#id_motivation").fadeIn();
-                }
+            
             });
             
             // Deal with the submission of this form:
@@ -37,17 +20,7 @@ $(document).ready(function(){
                 var motivation = $("#id_motivation").val();
                 var CSRFTOKEN = $("#csrfmiddlewaretoken").val();
                 
-                // Deal with case of anymous users are not shown the
-                // keep_private checkbox. (Just try catch and default to false.)
-                // TODO, check this, look into the server side as well.
-                try {
-                    var keep_private = $("#id_keep_private").val();
-                } catch(err){
-                    var keep_private = false;
-                }
-
                 var data = {
-                    'motivation' : motivation, 
                     'vote' : vote,
                     'csrfmiddlewaretoken' : CSRFTOKEN,
                     'issue_no' : issue_no,
@@ -57,14 +30,7 @@ $(document).ready(function(){
                 var afterVote = function(xhr, textStatus){
                     // This function updates the user interface after an 
                     // asynchronous javascript request to vote is handled by the
-                    // Emocracy server.
                     
-                    // Following is only save if no user submitted data is send
-                    // by the server --- because that might contain malicious 
-                    // javascript, which clearly should not be eval()-ed !
-                    
-                    // Look into also updating the userprofile information on
-                    // the current page.
                     eval("var jsonData = " + xhr.responseText + ";");
                      
                     if (textStatus == "success"){
@@ -88,11 +54,9 @@ $(document).ready(function(){
                     complete : afterVote,
                     dataType : "json",
                 });
-                
                 return false;
             });
         });
-        
         return false;
     });
     
