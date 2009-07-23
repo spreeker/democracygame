891fcf7fc9392f8f42f6b4ea91ea97d327e32252
spreeker <stephan@preeker.net>
2009-07-23 21:37:48 +0200

    improved usability dashboard

--- democracy/dashboard/urls.py
+++ democracy/dashboard/urls.py
@@ -6,5 +6,6 @@ from democracy.dashboard.views import index
 
 urlpatterns = patterns('',
     url(r'^issue/vote/(?P<issue_id>\d+)$', 'dashboard.views.record_vote', name='vote' ),
+    url(r'^issue/multiply/(?P<issue_id>\d+)$', 'dashboard.views.record_multiply', name='multiply' ),
     url(r'^$', index , name='dashboard'),
 )
