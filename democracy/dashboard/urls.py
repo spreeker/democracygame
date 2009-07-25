09b15ef7d61b4330f392662a47c7526671b5f93b
spreeker <stephan@preeker.net>
2009-07-25 11:37:32 +0200

    improved dashboard

--- democracy/dashboard/urls.py
+++ democracy/dashboard/urls.py
@@ -7,5 +7,6 @@ from democracy.dashboard.views import index
 urlpatterns = patterns('',
     url(r'^issue/vote/(?P<issue_id>\d+)$', 'dashboard.views.record_vote', name='vote' ),
     url(r'^issue/multiply/(?P<issue_id>\d+)$', 'dashboard.views.record_multiply', name='multiply' ),
+    url(r'^issue/(?P<sortorder>\w+)/$', index , name='dashboard_order'),
     url(r'^$', index , name='dashboard'),
 )
