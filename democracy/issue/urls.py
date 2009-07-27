ce76e40603ef3398353a2abadce2cb96161213e7
Stephan <stephan@preeker.net>
2009-07-27 03:11:58 +0200

    minor settings

--- democracy/issue/urls.py
+++ democracy/issue/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls.defaults import *
 from issue.models import Issue
+from django.views.decorators.cache import cache_page
+
+from views import issue_list
 
 urlpatterns = patterns('issue.views',
     url(r'^vote/(?P<issue_id>\d+)$', 'record_vote', name='vote' ),
     url(r'^multiply/(?P<issue_id>\d+)$', 'record_multiply', name='multiply' ),
-    url(r'^(?P<sortorder>\w+)/$', 'issue_list' , name='issue_list'),
+    #url(r'^(?P<sortorder>\w+)/$', cache_page(issue_list, 1*10) , name='issue_list'),
+    url(r'^(?P<sortorder>\w+)/$', issue_list , name='issue_list'),
 )
