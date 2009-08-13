14ed37b905ab14c641305f6da6a0ad0ac7dd5dee
Conrado <conrado@buhrer.net>
2009-08-13 08:26:09 +0200

    dirty branch, will clean soon... ajax implemented

--- thirdparty/ajax/urls.py
+++ thirdparty/ajax/urls.py
@@ -16,4 +16,12 @@ urlpatterns = patterns('ajax.views',
     url(r'^issue/(?P<issueid>\d+)/votes/$',
         view='ajax_get_issue_votes',
         name='ajax_get_issue_votes'),
+
+    url(r'^issues/(?P<sortorder>\w+)/$',
+        view='ajax_get_issues_list_ordered',
+        name='ajax_get_issues_list_ordered'),
+
+    url(r'^issues/(?P<sortorder>\w+)\.page/(?P<page>\d+)/$',
+        view='ajax_get_issues_list_ordered',
+        name='ajax_get_issues_list_ordered'),
 )
