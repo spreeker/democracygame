1b17d14260884f869012dc33519ecab9dc5607d9
Stephan Preeker <stephan@preeker.net>
2010-09-07 01:28:04 +0200

    found uncommited form bug

--- democracy/profiles/forms.py
+++ democracy/profiles/forms.py
@@ -20,12 +20,11 @@ class UserSearchForm(forms.Form):
     search_string = forms.CharField(max_length = 30, initial = _(u'Username'))
 
 class RegistrationTermsOfServiceUniqueEmail(
-        RegistrationFormTermsOfService, 
         RegistrationFormUniqueEmail
         ):
         """
         Subclass of RegistrationForms which enforces
-        both terms of service and an unique email.
+        an unique email.
         """
 
 
