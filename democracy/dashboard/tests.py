3b2bb3a5621f4121dd0d47987073450df1470e02
Stephan Preeker <stephan@preeker.net>
2010-10-14 14:39:02 +0200

    tried to do anonymous vote test. still not done.

--- democracy/dashboard/tests.py
+++ democracy/dashboard/tests.py
@@ -1,9 +1,11 @@
 from django.core.urlresolvers import reverse 
 from django.contrib.auth.models import User
 from django.test import TestCase 
+
 from gamelogic.tests import TestActionData 
 from issue.models import Issue
 from voting.models import Vote
+
 
 class IndexTest(TestCase):
     """
@@ -11,13 +13,12 @@ class IndexTest(TestCase):
     """ 
     def setUp(self):
         super(IndexTest , self).setUp()
-        u = User.objects.create_user("test","s@p.com","test")
-        self.i = Issue( title="test" , body="test", user=u )
+        u = User.objects.create_user("test", "s@p.com", "test")
+        self.i = Issue(title="test", body="test", user=u)
         self.i.save()
 
     def tearDown(self):
         super(IndexTest ,self).tearDown()
-
 
     def test_smoke_test(self):
         """ smoke test loading the basic pages should just work"""
@@ -30,7 +31,7 @@ class IndexTest(TestCase):
             response = self.client.get(url)
             self.assertEqual(response.status_code , 200)
 
-    def test_dashboard_test(self):
+    def test_issue_sort(self):
         """ smoke test views """
         self.assertEquals(
             self.client.login(username='test', password='test'), True) 
@@ -48,17 +49,18 @@ class IndexTest(TestCase):
             self.assertEqual(response.status_code , 200)
 
     def test_anonymous_votes(self):
+        #TODO ANYMOUS SESSION STORE SUCKS.
+        #self.client.login(username='test', password='test')
+        self.client.get('/')
         url = reverse("vote", args=[self.i.pk] )
         response = self.client.post(url, direction=1)
         self.assertEqual(response.status_code, 302)
         #annonymous sessions are not testable YET.
-
-        #url = reverse('issue_list' , args=["new"])
-        #can't acces an anonymous session.. *sigh* BUG 17-8-2009
+        url = reverse('issue_list' , args=["new"])
         #so we just check response content.
-        #response = self.client.get(url)
+        response = self.client.get(url)
+        session = self.client.session
 
         #self.assertContains(response, "<div class=\"for\"> 1</div>")
-
 
     ##TODO test_vote? test_post issue?
